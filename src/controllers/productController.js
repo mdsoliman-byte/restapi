71c8b510eea18fccb9d99a6c3b16bb3cdb3a7429
mdsoliman-me <contact.mdsoliman@gmail.com>
2022-09-17 19:52:34 +0600

    create data

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -1,4 +1,16 @@
+const productModel = require("../models/productsModel")
+
 const InsertData = (req, res) => {
+    const reqBody = req.body;
+    productModel.create(reqBody, (err, data) => {
+        if (err) {
+            res.status(404).json({ status: "Data NOt Found ", daat: err })
+        } else {
+            res.status(201).json({ status: "Success ", data: data })
+        }
+
+    })
+
 
 }
 
