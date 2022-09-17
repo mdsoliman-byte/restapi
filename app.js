acc000bdd11ef6cdbdab52ce2aa66db72b605969
mdsoliman-me <contact.mdsoliman@gmail.com>
2022-09-17 16:20:48 +0600

    config

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = new express()
+
+
+
+
+
+
+
+
+
+module.exports = app
+
+
+
---
+++ index.js
@@ -0,0 +1,8 @@
+const app = require("./app")
+const env = require('dotenv').config({path:"./config.env"})
+const PORT = process.env.WEB_PORT || 5000;
+
+
+app.listen(PORT, () => {
+    console.log(`App Running ${PORT}`)
+})
