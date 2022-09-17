7e8bac203a49358449a1dd046d4a5f261fe05b2c
mdsoliman-me <contact.mdsoliman@gmail.com>
2022-09-17 16:58:08 +0600

    mongose Schima Create

---
+++ src/models/productsModel.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+
+const DataSchima = mongoose.Schema({
+    Name: String,
+    Roll: String,
+    Class: String,
+    Remark: String
+})
+
+const productModel = mongoose.model("Product", DataSchima);
+module.exports = productModel
