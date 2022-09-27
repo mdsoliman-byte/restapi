453063f9a9604a767f50d25adeb83affd7920711
mdsoliman-me <contact.mdsoliman@gmail.com>
2022-09-27 22:46:09 +0600

    custome validation function

--- src/models/productsModel.js
+++ src/models/productsModel.js
@@ -1,10 +1,23 @@
 const mongoose = require("mongoose");
 const DataSchima = mongoose.Schema({
     Name: { type: String, unique: true },
+    Mobile: {
+        type: String,
+        validate: {
+            validator: function (value) {
+                if (value.length === 11) {
+                    return true
+                } else {
+                    return false
+                }
+            },
+            message: "11 Digit Mobile Number Required"
+        }
+    },
     Roll: {
         type: Number,
-        min: [6, `minum value 6 , maximum value 12, your value is {VALUE}`],
-        max: [12, `minum value 6 , maximum value 12, your value is {VALUE}`]
+        // min: [6, `minum value 6 , maximum value 12, your value is {VALUE}`],
+        // max: [12, `minum value 6 , maximum value 12, your value is {VALUE}`]
     },
     Class: { type: String, required: true },
     Remark: { type: String, default: "No Remark" }
