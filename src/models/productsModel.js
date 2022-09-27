dd71bcc0d352a7f4a342df6460fed4aea70c69bd
mdsoliman-me <contact.mdsoliman@gmail.com>
2022-09-27 21:47:13 +0600

    mongose validation

--- src/models/productsModel.js
+++ src/models/productsModel.js
@@ -1,9 +1,9 @@
 const mongoose = require("mongoose");
 const DataSchima = mongoose.Schema({
-    Name: String,
-    Roll: String,
-    Class: String,
+    Name: { type: String, unique: true },
+    Roll: { type: Number },
+    Class: { type: String, required: true },
     Remark: { type: String, default: "No Remark" }
-} ,{versionKey: false})
+}, { versionKey: false })
 const productModel = mongoose.model("products", DataSchima);
 module.exports = productModel;
