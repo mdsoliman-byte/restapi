bbc17dfeccb760c2e9c0beda83c9b91aceacfef1
mdsoliman-me <contact.mdsoliman@gmail.com>
2022-09-27 22:35:32 +0600

    custom error

--- src/models/productsModel.js
+++ src/models/productsModel.js
@@ -1,7 +1,11 @@
 const mongoose = require("mongoose");
 const DataSchima = mongoose.Schema({
     Name: { type: String, unique: true },
-    Roll: { type: Number, min: 10 },
+    Roll: {
+        type: Number,
+        min: [6, `minum value 6 , maximum value 12, your value is {VALUE}`],
+        max: [12, `minum value 6 , maximum value 12, your value is {VALUE}`]
+    },
     Class: { type: String, required: true },
     Remark: { type: String, default: "No Remark" }
 }, { versionKey: false })
