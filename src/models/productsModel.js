83ac6b7cde4891424624706fb0af46c658610017
mdsoliman-me <contact.mdsoliman@gmail.com>
2022-09-27 21:53:37 +0600

    min , max ,, value required

--- src/models/productsModel.js
+++ src/models/productsModel.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const DataSchima = mongoose.Schema({
     Name: { type: String, unique: true },
-    Roll: { type: Number },
+    Roll: { type: Number, min: 10 },
     Class: { type: String, required: true },
     Remark: { type: String, default: "No Remark" }
 }, { versionKey: false })
